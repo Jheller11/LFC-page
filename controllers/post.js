32e52f47808d650c37a1ba2e5f8a1b9fe99407a6
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-03-04 18:49:59 -0500

    add show view + link to show view in index

--- controllers/post.js
+++ controllers/post.js
@@ -2,9 +2,17 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
 
+// index view listing all posts
 router.get('/', (req, res) => {
   Post.find({}).then(posts => {
     res.render('post/index', { posts })
+  })
+})
+
+// show view (commenting feature to be added here)
+router.get('/:id', (req, res) => {
+  Post.findOne({ _id: req.params.id }).then(post => {
+    res.render('post/show', post)
   })
 })
 
