d1f5eb067026f7f06d4eaa01082af39504861628
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-03-04 16:12:54 -0500

    add connection to db

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => res.render('index.hbs'))
+
 app.listen(3000, () => console.log('server running'))
