ee1a3a33d77c7783afaf252e1ad6c252a6b566dc
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-03-04 16:34:21 -0500

    build controller and test, require in index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const hbs = require('hbs')
+const parser = require('body-parser')
+const postController = require('./controllers/post')
+
+app.use(parser.urlencoded({ extended: true }))
 
 app.set('view engine', 'hbs')
+
+app.use('/posts', postController)
 
 app.get('/', (req, res) => res.render('index.hbs'))
 
